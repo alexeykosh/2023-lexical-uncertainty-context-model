84085d7ab832500047a2526175e218772cdb5aec
alexeykosh <alexeykochevoy@gmail.com>
2023-05-16 11:09:27 +0200

    small tweaks in the naive model

--- src/model/model.py
+++ src/model/model.py
@@ -6,8 +6,11 @@ import numpy as np
 import pandas as pd
 from util import (generate_lexicons, 
                   lex_to_str)
+from scipy.stats import entropy
 
 np.seterr(divide='ignore', invalid='ignore')
+
+NOISE = 1e-10
 
 
 class BaseRSA:
@@ -31,6 +34,11 @@ class BaseRSA:
             return self.normalize(L * self.prior)
         return self.normalize(L * self.prior * self.contexts[c])
     
+    def S_0(self, L, c):
+        if c is None:
+            return self.normalize(L.transpose(0, 2, 1) )
+        return self.normalize(np.exp(self.safelog(L.transpose(0, 2, 1)) - self.C))
+    
     def S_p(self, L, c=None):
         if c is None:
             return self.normalize(np.exp(self.alpha * (self.safelog(self.L_0(L, c).transpose(0, 2, 1)) - self.C)))
@@ -39,49 +47,84 @@ class BaseRSA:
     def L_p(self, L, c=None):
         if c is None:
             return self.normalize(self.S_p(L, c).transpose(0, 2, 1) * self.prior)
-        return self.normalize(self.S_p(L, c).transpose(0, 2, 1) * self.prior * self.contexts[c])
+        return self.normalize((self.S_p(L, c).transpose(0, 2, 1) * self.prior * self.contexts[c])) 
 
 class Agent(BaseRSA):
-    def __init__(self, alpha, prior, C, n_words, n_meanings, contexts):
+    def __init__(self, alpha, prior, C, n_words, n_meanings, contexts, model="RSA"):
         super().__init__(alpha, prior, C, contexts)
         self.Lexicons = generate_lexicons(n_words, n_meanings)
         self.prob_lexicon = np.ones(len(self.Lexicons)) / len(self.Lexicons)
         self.n_words = n_words
         self.n_meanings = n_meanings
+        self.model = model
     
     def speaker(self, m, c):
         # index of the lexicon with the highest probability given prob_lexicon
-        lexicon_idx = np.random.choice(np.arange(len(self.Lexicons)), p=self.prob_lexicon)
-        return self.S_p(self.Lexicons, c)[lexicon_idx][m].argmax()
+        if self.model == "RSA":
+            lexicon_idx = np.random.choice(np.arange(len(self.Lexicons)), p=self.prob_lexicon)
+            # return self.S_p(self.Lexicons, c)[lexicon_idx][m].argmax()
+            return np.random.choice(np.arange(self.n_words), p=self.S_p(self.Lexicons, c)[lexicon_idx][m])
+        elif self.model == "naive":
+            lexicon_idx = np.random.choice(np.arange(len(self.Lexicons)), p=self.prob_lexicon)
+            return np.random.choice(np.arange(self.n_words), p=self.S_0(self.Lexicons, c)[lexicon_idx][m])
     
     def listener(self, w, c):
         # index of the lexicon with the highest probability given prob_lexicon
-        lexicon_idx = np.random.choice(np.arange(len(self.Lexicons)), p=self.prob_lexicon)
-        return self.L_p(self.Lexicons, c)[lexicon_idx][w].argmax()
+        if self.model == "RSA":
+            lexicon_idx = np.random.choice(np.arange(len(self.Lexicons)), p=self.prob_lexicon)
+            # return self.L_p(self.Lexicons, c)[lexicon_idx][w].argmax()
+            if np.sum(self.L_p(self.Lexicons, c)[lexicon_idx][w]) < 1:
+                return np.random.choice(np.arange(self.n_meanings), p=[1 / self.n_meanings] * self.n_meanings)
+            else:
+                return np.random.choice(np.arange(self.n_meanings), p=self.L_p(self.Lexicons, c)[lexicon_idx][w])
+        elif self.model == "naive":
+            lexicon_idx = np.random.choice(np.arange(len(self.Lexicons)), p=self.prob_lexicon)
+            if np.sum(self.L_0(self.Lexicons, c)[lexicon_idx][w]) < 1:
+                return np.random.choice(np.arange(self.n_meanings), p=[1 / self.n_meanings] * self.n_meanings)
+            else:
+                return np.random.choice(np.arange(self.n_meanings), p=self.L_0(self.Lexicons, c)[lexicon_idx][w])
+
     
     def update(self, w, m, c, correct, role):
         if role == "speaker":
-            if correct:
-                self.prob_lexicon = self.normalize(self.S_p(self.Lexicons, c)[:, m, w] * self.prob_lexicon + 1e-5)
-            else:
-                self.prob_lexicon = self.normalize(self.S_p(self.Lexicons, c)[:, m, 1 - w] * self.prob_lexicon + 1e-5)
+            if self.model == "RSA":
+                if correct:
+                    self.prob_lexicon = self.normalize(self.S_p(self.Lexicons, c)[:, m, w] * self.prob_lexicon + NOISE)
+                else:
+                    self.prob_lexicon = self.normalize(self.S_p(self.Lexicons, c)[:, m, 1 - w] * self.prob_lexicon + NOISE)
+            elif self.model == "naive":
+                if correct:
+                    self.prob_lexicon = self.normalize(self.S_0(self.Lexicons, c)[:, m, w] * self.prob_lexicon + NOISE)
+                else:
+                    self.prob_lexicon = self.normalize(self.S_0(self.Lexicons, c)[:, m, 1 - w] * self.prob_lexicon + NOISE)
         elif role == "listener":
-            if correct:
-                self.prob_lexicon = self.normalize(self.L_p(self.Lexicons, c)[:, w, m] 
-                * self.prob_lexicon + 1e-5)
-            else:
-                meanings = range(self.n_meanings)
-                for meaning in meanings:
-                    if meaning != m:
-                        self.prob_lexicon = self.normalize(self.L_p(self.Lexicons, c)[:, w, meaning]
-                         * self.prob_lexicon + 1e-5)
+            if self.model == "RSA":
+                if correct:
+                    self.prob_lexicon = self.normalize(self.L_p(self.Lexicons, c)[:, w, m] 
+                    * self.prob_lexicon + NOISE)
+                else:
+                    meanings = range(self.n_meanings)
+                    for meaning in meanings:
+                        if meaning != m:
+                            self.prob_lexicon = self.normalize(self.L_p(self.Lexicons, c)[:, w, meaning]
+                            * self.prob_lexicon + NOISE)
+            elif self.model == "naive":
+                if correct:
+                    self.prob_lexicon = self.normalize(self.L_0(self.Lexicons, c)[:, w, m] 
+                    * self.prob_lexicon + NOISE)
+                else:
+                    meanings = range(self.n_meanings)
+                    for meaning in meanings:
+                        if meaning != m:
+                            self.prob_lexicon = self.normalize(self.L_0(self.Lexicons, c)[:, w, meaning]
+                            * self.prob_lexicon + NOISE)
 
     def max_prob_L(self):
         return lex_to_str(self.Lexicons[np.argmax(self.prob_lexicon)])
     
 
 class Experiment:
-    def __init__(self, alpha, prior, C, contexts, n_iter, n_rounds):
+    def __init__(self, alpha, prior, C, contexts, n_iter, n_rounds, model):
         self.n_iter = n_iter
         self.n_rounds = n_rounds
 
@@ -92,6 +135,7 @@ class Experiment:
         self.prior = prior
         self.C = C
         self.contexts = contexts
+        self.model = model
         
 
     def one_round(self, a, b, m, c, i, r):
@@ -106,11 +150,13 @@ class Experiment:
         self.logs[i][r]['correct'] = 1 if (m == g) else 0
         self.logs[i][r]['lexicon_a'] = a.max_prob_L()
         self.logs[i][r]['lexicon_b'] = b.max_prob_L()
+        self.logs[i][r]['entropy_a'] = entropy(a.prob_lexicon)
+        self.logs[i][r]['entropy_b'] = entropy(a.prob_lexicon)
     
     def run(self):
         for i in range(self.n_iter):
-            self.agents = [Agent(alpha=self.alpha, prior=self.prior, C=self.C, n_words=2, n_meanings=3, contexts=self.contexts),
-                            Agent(alpha=self.alpha, prior=self.prior, C=self.C, n_words=2, n_meanings=3, contexts=self.contexts)]
+            self.agents = [Agent(alpha=self.alpha, prior=self.prior, C=self.C, n_words=2, n_meanings=3, contexts=self.contexts, model=self.model),
+                            Agent(alpha=self.alpha, prior=self.prior, C=self.C, n_words=2, n_meanings=3, contexts=self.contexts, model=self.model)]
             for r in range(self.n_rounds):
                 m = np.random.choice([0, 1, 2], p=self.prior)
                 # if meaning is 0, context is either 0 or 1, if 1, context is 0, if 2 context is 1
@@ -129,13 +175,16 @@ class Experiment:
             self.group_posterior.append(self.agents[0].prob_lexicon)
             self.group_posterior.append(self.agents[1].prob_lexicon)
                         
-    def save(self):
+    def save(self, rd=False):
         df = pd.DataFrame.from_dict({(i, r): self.logs[i][r]
                                         for i in self.logs.keys()
                                         for r in self.logs[i].keys()},
                                         orient='index').reset_index()
-        df.columns = ['trial', 'round', 'meaning', 'context', 'word', 'word_length', 'guess', 'correct', 'lexicon_a', 'lexicon_b']
-        df.to_csv(f'src/data/logs/logs-{self.n_iter}-{int(self.alpha)}.csv', index=False)
+        df.columns = ['trial', 'round', 'meaning', 'context', 'word', 'word_length', 'guess', 'correct', 'lexicon_a', 'lexicon_b', 'entropy_a', 'entropy_b']
+        if not rd:
+            df.to_csv(f'src/data/logs/logs-{self.n_iter}-{int(self.alpha)}.csv', index=False)
+        else:
+            return df
           
 
 if __name__ == '__main__':
@@ -144,11 +193,13 @@ if __name__ == '__main__':
     parser.add_argument('--alpha', type=float)
     parser.add_argument('--costs', type=str)
     parser.add_argument('--priors', type=str)
+    parser.add_argument('--model', type=str)
     args = parser.parse_args()
+    args.model = "RSA" if args.model == "rsa" else "naive"
     args.costs = np.array(args.costs.split(',')).astype(float)
     args.priors = np.array(args.priors.split(',')).astype(float)
     prior = args.priors / np.sum(args.priors)
     contexts = np.array([[1, 1, 0], [1, 0, 1]])
-    exp = Experiment(args.alpha, prior, args.costs, contexts, args.n_iter, 100)
+    exp = Experiment(args.alpha, prior, args.costs, contexts, args.n_iter, 50, args.model)
     exp.run()
     exp.save()
